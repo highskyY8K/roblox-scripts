9604eb58646dfe267e781d6b6d2bdeb7b8975de0
highskyY8K <163911915+highskyY8K@users.noreply.github.com>
2025-04-13 15:21:44 +0100

    Update Quiet game.lua

--- internalbuiltinscripts/Quiet game.lua
+++ internalbuiltinscripts/Quiet game.lua
@@ -3,13 +3,13 @@ game.workspace.ChildAdded:Connect(function(hourglass)
 		wait(0.1)
         for _, part in pairs(hourglass:GetChildren()) do
             if part.Name == "PrimaryPart" then
-				      part.Transparency = 1
-				      part.CanCollide = false
+				part.Transparency = 1
+				part.CanCollide = false
                 part.CFrame = game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame
             elseif part:IsA("BasePart") or part:IsA("Mesh") then
-				      part.Transparency = 1
-				      part.CanCollide = false
-			      end
+				part.Transparency = 1
+				part.CanCollide = false
+			end
         end
     end
 end)
