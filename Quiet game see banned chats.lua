05e3a3685c56f1b0e571d7136aeaec639f097261
highskyY8K <163911915+highskyY8K@users.noreply.github.com>
2025-08-12 15:27:11 +0100

    Update Quiet game see banned chats.lua

--- Quiet game see banned chats.lua
+++ Quiet game see banned chats.lua
@@ -4,17 +4,29 @@ local function DisplaySystemMessage(Message)
 end
 
 for _,Player in pairs(game.Players:GetPlayers()) do
-	if Player.Name~=game.Players.LocalPlayer.Name and Player:GetAttribute("Banned")==true then
+	local colour = 255 - ((#Player.Name) * 2)
+	if Player.Name~=game.Players.LocalPlayer.Name then
 		Player.Chatted:Connect(function(Message)
-			DisplaySystemMessage("<font color='rgb(0,255,255)'>"..Player.Name..": </font>"..Message)
+			if Player:GetAttribute("Banned")==true then
+				DisplaySystemMessage("<font color='rgb(0," .. colour .. "," .. colour .. ")'>"..Player.DisplayName..": </font>"..Message)
+			else
+				if string.sub(Message,1,3)=="/t "then
+					DisplaySystemMessage("<font color='rgb(0," .. colour .. "," .. colour .. ")'>"..Player.DisplayName..": </font>"..Message)
+				end
+			end	
 		end)
 	end
 end
 
 game.Players.PlayerAdded:Connect(function(Player)
-	if Player:GetAttribute("Banned")==true then
-		Player.Chatted:Connect(function(Message)
-			DisplaySystemMessage("<font color='rgb(0,255,255)'>"..Player.Name..": </font>"..Message)
-		end)
-	end
+	local colour = 255 - ((#Player.Name) * 2)
+	Player.Chatted:Connect(function(Message)
+		if Player:GetAttribute("Banned")==true then
+			DisplaySystemMessage("<font color='rgb(0," .. colour .. "," .. colour .. ")'>"..Player.DisplayName..": </font>"..Message)
+		else
+			if string.sub(Message,1,3)=="/t "then
+				DisplaySystemMessage("<font color='rgb(0," .. colour .. "," .. colour .. ")'>"..Player.DisplayName..": </font>"..Message)
+			end
+		end	
+	end)
 end)
